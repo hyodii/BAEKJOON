702e05f17b2b64eb7db9da349a043cfe896973bd
HyoJin Jeong <87711421+hyodii@users.noreply.github.com>
2021-08-30 00:00:33 +0900

    Add files via upload

---
+++ BAEKJOON/10718.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+ 
+    }
+}
+/*
+대'한'육군을 대'학'육군이라고 써서 몇 번을 틀린거니....
+저 오타가 왤케 안보이던지...
+그럴일이냐구..
+*/

2775a31dcc6c51b0647543280338f081d227f84f
HyoJin Jeong <87711421+hyodii@users.noreply.github.com>
2021-08-30 13:23:31 +0900

    Cat
    고양이 출력

---
+++ BAEKJOON/10171.java
@@ -0,0 +1,21 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+
+/*
+고양이 출력
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+
+역슬래쉬 \는 단독으로 출력못한다!
+\\두번 입력해야함!!
+*/

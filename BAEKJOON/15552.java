b159fc03d7459ec21a52448f9a7af0c7a898de24
HyoJin Jeong <87711421+hyodii@users.noreply.github.com>
2021-09-11 23:19:44 +0900

    fast A + B add use StringBuilder

--- BAEKJOON/15552.java
+++ BAEKJOON/15552.java
@@ -47,6 +47,37 @@ public class Main {
  
 	}
 }
+
+
+/*
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+        
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+        
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append('\n');
+		}
+		br.close();
+ 
+		System.out.println(sb);
+ 
+	}
+}
+*/
+
 /*
 5
 1 1
@@ -61,3 +92,6 @@ public class Main {
 10
 계속하려면 아무 키나 누르십시오 . . .
 */
+
+
+// BufferedWriter 보다 StringBuilder가 더 빠르다!!
